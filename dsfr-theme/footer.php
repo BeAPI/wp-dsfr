1e3991e82cddd5a770a3a102dce382e829c2177b
Nicolas Langle <nlangle@beapi.fr>
2024-05-22 15:34:11 +0200

    feat (theme): add follow section in footer

--- dsfr-theme/footer.php
+++ dsfr-theme/footer.php
@@ -1,5 +1,6 @@
 		</main>
 		<?php
+		get_template_part( 'components/parts/common/follow' );
 		get_template_part( 'components/parts/footer/footer' );
 		get_template_part( 'components/parts/common/theme-modal' );
 		wp_footer();
