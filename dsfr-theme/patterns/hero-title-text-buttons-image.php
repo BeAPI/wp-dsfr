cd09d621c36b64063b8a6ce5a16611743172d6cc
Nicolas Langle <nlangle@beapi.fr>
2024-07-18 17:19:15 +0200

    feat (theme): update pattern hero icon style again

--- dsfr-theme/patterns/hero-title-text-buttons-image.php
+++ dsfr-theme/patterns/hero-title-text-buttons-image.php
@@ -6,8 +6,8 @@
  * Viewport width: 1248px
  */
 ?>
-<!-- wp:group {"tagName":"header","metadata":{"name":"Hero avec titre, texte, boutons et image","categories":["hero"],"patternName":"dsfr/hero-title-text-buttons-image-with-background"},"align":"full","className":"wp-block-group\u002d\u002dhero","layout":{"type":"constrained"}} -->
-<header class="wp-block-group alignfull wp-block-group--hero">
+<!-- wp:group {"tagName":"header","metadata":{"name":"Hero avec titre, texte, boutons et image","categories":["hero"],"patternName":"dsfr/hero-title-text-buttons-image-with-background"},"align":"full","className":"wp-block-group\u002d\u002dhero wp-block-group\u002d\u002dhero-icon","layout":{"type":"constrained"}} -->
+<header class="wp-block-group alignfull wp-block-group--hero wp-block-group--hero-icon">
 	<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide">
 		<!-- wp:column {"width":"66.66%"} -->
@@ -40,8 +40,8 @@
 
 		<!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
 		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%">
-			<!-- wp:image {"className":"wp-block-group--hero__icon","width":"180px","height":"180px","scale":"cover","align":"center"} -->
-			<figure class="wp-block-image aligncenter is-resized wp-block-group--hero__icon"><img alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
+			<!-- wp:image {"width":"180px","height":"180px","scale":"cover","align":"center"} -->
+			<figure class="wp-block-image aligncenter is-resized"><img alt="" style="object-fit:cover;width:180px;height:180px" /></figure>
 			<!-- /wp:image -->
 		</div>
 		<!-- /wp:column -->
