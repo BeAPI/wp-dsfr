7be1b046f26135b1ca1f8b76de16cdb3eeee3002
Nicolas Langle <nlangle@beapi.fr>
2024-09-23 11:24:34 +0200

    fix (theme): 404 br

--- wp-dsfr-theme/404.php
+++ wp-dsfr-theme/404.php
@@ -9,7 +9,7 @@ get_header();
 			<h1><?php esc_html_e( 'Page non trouvée', 'wp-dsfr-theme' ); ?></h1>
 			<p class="fr-text--sm fr-mb-3w"><?php esc_html_e( 'Erreur 404', 'wp-dsfr-theme' ); ?></p>
 			<p class="fr-text--lead fr-mb-3w"><?php esc_html_e( 'La page que vous cherchez est introuvable. Excusez-nous pour la gène occasionnée.', 'wp-dsfr-theme' ); ?></p>
-			<p class="fr-text--sm fr-mb-5w"><?php esc_html_e( 'Si vous avez tapé l\'adresse web dans le navigateur, vérifiez qu\'elle est correcte. La page n’est peut-être plus disponible. <br>Dans ce cas, pour continuer votre visite vous pouvez consulter notre page d’accueil, ou effectuer une recherche avec notre moteur de recherche en haut de page. <br>Sinon contactez-nous pour que l’on puisse vous rediriger vers la bonne information.', 'wp-dsfr-theme' ); ?></p>
+			<p class="fr-text--sm fr-mb-5w"><?php echo wp_kses_post( __( 'Si vous avez tapé l\'adresse web dans le navigateur, vérifiez qu\'elle est correcte. La page n’est peut-être plus disponible. <br>Dans ce cas, pour continuer votre visite vous pouvez consulter notre page d’accueil, ou effectuer une recherche avec notre moteur de recherche en haut de page. <br>Sinon contactez-nous pour que l’on puisse vous rediriger vers la bonne information.', 'wp-dsfr-theme' ) ); ?></p>
 			<ul class="fr-btns-group fr-btns-group--inline-md">
 				<li><a class="fr-btn" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Page d\'accueil', 'wp-dsfr-theme' ); ?></a></li>
 			</ul>
